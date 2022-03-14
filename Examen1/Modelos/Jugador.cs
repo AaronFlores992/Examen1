9bbec8fb0c42d73a7cfe9f7f65e5edb9725dba70
Aaron Flores <aaron@gmail.com>
2022-03-14 11:39:19 -0600

    se agregan clases jugador y ruleta

---
+++ Examen1/Modelos/Jugador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1.Modelos
+{
+    internal class Jugador
+    {
+        private String _nombre;
+        private int _id;
+        private int _dineroActual;
+        private int _dineroPerdido;
+
+        public Jugador(String nombre, int id, int dineroActual, int dineroPerdido) { 
+            this._nombre = nombre;
+            this._id = id;
+            this._dineroActual = 300;
+            this._dineroPerdido = 0;
+        }
+
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public int dineroActual
+        {
+            get { return _dineroActual; }
+            set { _dineroActual = value; }
+        }
+
+        public int dineroPerdido
+        {
+            get { return _dineroActual; }
+            set { _dineroPerdido = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Jugador: {_nombre}";
+        }
+    }
+}
---
+++ Examen1/Modelos/Ruleta.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1.Modelos
+{
+    internal class Ruleta
+    {
+        private List<int> _rojos;
+        private List<int> _negros;
+        private List<int> _contadorPos;
+        private int _giros;
+
+        public Ruleta(List<int> rojos, List<int> negros, List<int> contadorPos, int giros) {
+            this._rojos = new List<int>() {
+            1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34 ,36};
+            this._negros = new List<int>() {
+            2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33 ,35};
+            this._contadorPos = new List<int>();
+            this._giros = giros;
+        }
+
+        public List<int> rojos
+        {
+            get { return _rojos; }
+            set { _rojos = value; }
+        }
+
+        public List<int> negros
+        {
+            get { return _negros; }
+            set { _negros = value; }
+        }
+
+        public List<int> contadorPos
+        {
+            get { return _contadorPos; }
+            set { _contadorPos = value; }
+        }
+
+        public int giros
+        {
+            get { return _giros; }
+            set { _giros = value; }
+        }
+
+
+
+    }
+}
